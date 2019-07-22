d4d46912291e7ea9894c6a67cea608cdb4785fac
Jason Rogena <jason@rogena.me>
2019-07-22 14:12:30 +0300

    Add Testinfra tests
    Add Testinfra tests to check:
     - If the RapidpPro services start
     - If the RapidPro services are enabled
     - If the current app version symlink is created
     - The ownership of created directories
    
    Signed-off-by: Jason Rogena <jason@rogena.me>

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,9 +6,45 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_rapidpro_service(host):
+    rapidpro = host.service("rapidpro")
+    assert rapidpro.is_running
+    assert rapidpro.is_enabled
 
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+    celeryD = host.service("celeryd-rapidpro")
+    assert celeryD.is_running
+    assert celeryD.is_enabled
+
+    celeryBeat = host.service("celerybeat-rapidpro")
+    assert celeryBeat.is_running
+    assert celeryBeat.is_enabled
+
+
+def test_rapidpro_app_files(host):
+    appDir = host.file("/home/rapidpro/app")
+    assert appDir.exists
+    assert appDir.user == "rapidpro"
+    assert appDir.group == "rapidpro"
+    assert appDir.is_symlink
+    assert appDir.linked_to == "/home/rapidpro/app-versioned/v5.0.9"
+
+    appVersionedDir = host.file("/home/rapidpro/app-versioned/v5.0.9")
+    assert appVersionedDir.exists
+    assert appVersionedDir.user == "rapidpro"
+    assert appVersionedDir.group == "rapidpro"
+    assert appVersionedDir.is_directory
+    assert oct(appVersionedDir.mode) == "0o755"
+
+    mediaDir = host.file("/home/rapidpro/media")
+    assert mediaDir.exists
+    assert mediaDir.user == "rapidpro"
+    assert mediaDir.group == "rapidpro"
+    assert mediaDir.is_directory
+    assert oct(mediaDir.mode) == "0o775"
+
+    virtualenv = host.file("/home/rapidpro/.virtualenvs/rapidpro")
+    assert virtualenv.exists
+    assert virtualenv.user == "rapidpro"
+    assert virtualenv.group == "rapidpro"
+    assert virtualenv.is_directory
+    assert oct(virtualenv.mode) == "0o755"
